537a25d49f662f54c635914666e566c989f4f96c
Derek Olson <derekjosepholson@gmail.com>
2015-07-13 17:36:48 -0500

    cleaned up messages slightly

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -1,8 +1,10 @@
 // YOUR CODE HERE:
 var app = {};
+
 app.init = function(){
 
 }; 
+
 app.send = function(message){
   $.ajax({
     type: "POST",
@@ -10,7 +12,7 @@ app.send = function(message){
     data: JSON.stringify(message),
     contentType: 'application/json',
     success: function (data) {
-      console.log('chatterbox: Message sent');
+      console.log('chatterbox: Message sent' + JSON.stringify(data));
     },
     error: function (data) {
     // See: https://developer.mozilla.org/en-US/docs/Web/API/console.error
@@ -20,13 +22,37 @@ app.send = function(message){
   });
 
 };
+
 app.fetch = function(){
   $.ajax({
-    type: "GET"
+    type: "GET",
+    url: 'https://api.parse.com/1/classes/chatterbox',
+    success: function (data) {
+      var results = data.results;
+      for(var index = 0; index < results.length; index++){
+        app.addMessage(results[index]);
+      }
+
+    },
+    error: function (data) {
+    // See: https://developer.mozilla.org/en-US/docs/Web/API/console.error
+      console.error('chatterbox: Failed to recieve message');
+    }
+
   });
 };
-app.send({
-  username: 'Mel Brooks',
-  text: 'It\'s good to be the king',
-  roomname: 'lobby'
-});
+
+app.clearMessages = function() {
+  $("#chats").html('');
+};
+
+app.addMessage = function(message) {
+  $("#chats").append('<div class="chat">'    + '<span class="username">' +message.username+'</span>' +
+    ': ' + message.text + '@ ' + message.createdAt       + '</div>');
+  //app.addMessage(alert(1))
+};
+
+
+
+
+app.fetch();
